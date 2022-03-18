feb102171a966c48d4af67ead529717b2b870de5
albertocruz6 <alberto.cruz6@upr.edu>
2022-03-18 19:06:47 -0400

    Added another test to enphasize on how to write unit test

--- backend/tests/unit/test_users_controller.py
+++ backend/tests/unit/test_users_controller.py
@@ -4,6 +4,9 @@ import sys
 sys.path.insert(1, '/controller/')
 from controller.users import BaseUsers
 
+'''
+Command to run tests: python -m unittest tests/unit/test_users_controller.py
+'''
 class TestUserController(unittest.TestCase):
     def test_users_controller_login_map(self):
         expected = {
@@ -14,3 +17,15 @@ class TestUserController(unittest.TestCase):
         test_row = ('1', 'Bob', 'Strat')
         test_user = BaseUsers()
         self.assertAlmostEqual(expected, test_user.build_login_map_dict(test_row))
+
+    def test_users_controller_newuser_map(self):
+        expected = {
+            'userid' : '1',
+            'firstname': 'Bob',
+            'lastname': 'Strat',
+            'email': 'bob.strat@mail.com',
+            'legalAge': 'True'
+        }
+        test_row = ('1', 'Bob', 'Strat', 'bob.strat@mail.com', 'True')
+        test_user = BaseUsers()
+        self.assertAlmostEqual(expected, test_user.build_newuser_map_dict(test_row))
