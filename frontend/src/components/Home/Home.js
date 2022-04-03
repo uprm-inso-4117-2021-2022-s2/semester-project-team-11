bb1b30983f5149c24304b5df2989d18af9a76a0b
Peter L Santana <42743321+peter-santana@users.noreply.github.com>
2022-04-03 15:14:41 -0400

    added navbar for home

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
+import { Container, Col, Row, Form } from "react-bootstrap";
+import './Home.css';
+import logo from '../../../src/images/Stocks_GrowthCombined.png';
+// import NavBar from "frontend/src/components/Navbar/Navbar.js";
 
-export default function Home() {
-    return (
-        <div>Home</div>
-    )
+class Home extends Component {
+    render(){
+        return (
+            <>
+            <Navbar className="color-nav" >
+                
+              <Container>
+              <Navbar.Brand href="#home">
+                <img
+                alt=""
+                src={logo}
+                width="120"
+                height="50"
+                className="d-inline-block align-top"
+                    />{' '}
+              </Navbar.Brand>
+              <Nav className="ml-auto">
+                <Nav.Link href="#Register">Register</Nav.Link>
+                <Nav.Link href="#Login">Login</Nav.Link>
+              </Nav>
+              </Container>
+            </Navbar>
+          </>
+        );
+    }
 }
+
+export default Home;
