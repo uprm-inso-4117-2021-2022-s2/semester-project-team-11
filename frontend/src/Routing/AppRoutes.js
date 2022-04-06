f41aceff0b935e64db51f4239b11be5e7d01b4c3
fabiolarobles1 <fabiola.robles1@upr.edu>
2022-04-06 01:03:27 -0400

    fixed guarded routes

--- frontend/src/Routing/AppRoutes.js
+++ frontend/src/Routing/AppRoutes.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from '../components/Dashboard/Dashboard'
 import Home from '../components/Home/Home'
 import Register from '../components/Register/Register'
+import GuardedRoute from './GuardedRoutes'
 
 export default function AppRoutes() {
     return (
@@ -9,6 +11,7 @@ export default function AppRoutes() {
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/dashboard" element={<GuardedRoute component={Dashboard} />} />
             </Routes>
         </BrowserRouter>
     )
