5ed0dffcb3b9c5c94291d54c4aa0d3f14a7697a7
fabiolarobles1 <fabiola.robles1@upr.edu>
2022-04-07 18:49:46 -0400

    dashboard layout on progress

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,9 +1,138 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Nav, Navbar, Container } from 'react-bootstrap';
+import logo from '../../../src/images/Stocks_GrowthCombined.png';
+import axios from "axios";
+import { BsSearch } from "react-icons/bs";
+import { Table } from 'react-bootstrap';
+import graph from '../../images/grafiquita-1.png'
+import "./Dashboard.css"
 
 export default function Dashboard() {
+
+    const [searchWord, setSearchWord] = useState("");
+    const [searchResult, setSearchResult] = useState({})
+
+    const searchRequest = {
+        method: 'GET',
+        url: 'https://yh-finance.p.rapidapi.com/auto-complete',
+        params: { q: searchWord, region: 'US' },
+        headers: {
+            'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
+            'X-RapidAPI-Key': 'feac1ad6damshf3407a191632ab5p10c29djsnacfead750eb3'
+        }
+    };
+
+    const searchAction = (e) => {
+
+        if (e.type === "click" || e.key === 'Enter') {
+            axios.request(searchRequest).then(function (response) {
+                setSearchResult(response.data);
+                console.log(searchResult.quotes)
+            }).catch(function (error) {
+                console.error(error);
+            });
+        }
+    };
+
+    const searchBar =
+        <div className='dashboard-searchBar-container'>
+            <input type="text"
+                placeholder="Search for a keyword..."
+                value={searchWord}
+                name="searchWord"
+                onChange={(e) => setSearchWord(e.target.value)}
+                onKeyDown={searchAction}
+                style={{ height: 40, width: "80%", borderRadius: 15, borderColor: 'white', backgroundColor: "lightgray" }}
+            />
+            < BsSearch name='search' onClick={searchAction} style={{ margin: '5px' }} />
+        </div>;
+
     return (
-        <div>
-            Dashboard
-        </div>
+        <>
+            <Navbar className="color-nav" >
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img
+                            alt=""
+                            src={logo}
+                            width="120"
+                            height="50"
+                            className="d-inline-block align-top"
+                        />
+                    </Navbar.Brand>
+                    <Nav className="ml-auto">
+                        {/* buttons */}
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            <div className='dashboard-flex-container'>
+                <div className='dashboard-left-container'>
+                    <div className='dashboard-header-container'>
+                        {localStorage.getItem('fname')}'s Dashboard
+                    </div>
+                    <div className='dashboard-current-stock '>
+                        <img src={graph} alt="stock" />
+                    </div>
+                    <div className='dashboard-searchBar-container'>{searchBar}</div>
+
+
+
+                    {searchResult.quotes &&
+                        <Table responsive>
+                            <thead>
+                                <tr>
+                                    <th key={0}>Stock Results</th>
+                                </tr>
+                                <tr>
+                                    <td key={2}>Symbol</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {searchResult.quotes.map((item) => (
+                                    <tr>
+                                        <td key={item.key}>{item.symbol}</td>
+                                    </tr>
+                                ))}
+
+                            </tbody>
+                        </Table>
+                    }
+                    {searchResult.news &&
+                        <Table responsive>
+                            <thead>
+                                <tr>
+                                    <th key={1}>News Results</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {searchResult.news.map((item) => (
+                                    <tr>
+                                        <td key={item.key}>{item.link}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+                    }
+                </div>
+                <div className='dashboard-right-container'>
+                    {searchResult.news &&
+                        <Table>
+                            <thead>
+                                <tr>
+                                    <th key={3}>My Stocks</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {searchResult.news.map((item) => (
+                                    <tr>
+                                        <td key={item.key}>{item.link}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>}
+                </div>
+            </div>
+        </>
     )
 }
--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -5,61 +5,60 @@ import { Nav, Navbar, NavDropdown, Card, Button, Carousel } from 'react-bootstra
 import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
 import { Container, Col, Row, Form } from "react-bootstrap";
 import './Home.css';
-import {
-  MDBCarousel,
-  MDBCarouselInner,
-  MDBCarouselItem,
-  MDBView,
-  MDBContainer
-} from "mdbreact";
+// import {
+//   MDBCarousel,
+//   MDBCarouselInner,
+//   MDBCarouselItem,
+//   MDBView,
+//   MDBContainer
+// } from "mdbreact";
 import logo from '../../../src/images/Stocks_GrowthCombined.png';
 // import NavBar from "frontend/src/components/Navbar/Navbar.js";
 
 class Home extends Component {
-    render(){
-        return (
-            <>
-            <Navbar className="color-nav" >
-                
-              <Container>
-              <Navbar.Brand href="#home">
-                <img
+  render() {
+    return (
+      <>
+        <Navbar className="color-nav" >
+          <Container>
+            <Navbar.Brand href="#home">
+              <img
                 alt=""
                 src={logo}
                 width="120"
                 height="50"
                 className="d-inline-block align-top"
-                    />{' '}
-              </Navbar.Brand>
-              <Nav className="ml-auto">
-                <Nav.Link href="/register">Register</Nav.Link>
-                <Nav.Link href="/register">Login</Nav.Link>
-              </Nav>
-              </Container>
-            </Navbar>
+              />{' '}
+            </Navbar.Brand>
+            <Nav className="ml-auto">
+              <Nav.Link href="/register">Register</Nav.Link>
+              <Nav.Link href="/register">Login</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
 
 
-            <Carousel variant="dark">
-  <Carousel.Item interval={1000}>
-    
-    <Carousel.Caption>
-      <h3>News</h3>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item interval={500}>
-    <Carousel.Caption>
-      <h3>Educational Resources</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <Carousel.Caption>
-      <h3>Dashboard</h3>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
+        <Carousel variant="dark">
+          <Carousel.Item interval={1000}>
 
-{/* <MDBContainer>
+            <Carousel.Caption>
+              <h3>News</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={500}>
+            <Carousel.Caption>
+              <h3>Educational Resources</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Carousel.Caption>
+              <h3>Dashboard</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+
+        {/* <MDBContainer>
       <MDBCarousel
         activeItem={1}
         length={3}
@@ -102,9 +101,9 @@ class Home extends Component {
 
 
 
-          </>
-        );
-    }
+      </>
+    );
+  }
 }
 
 export default Home;
