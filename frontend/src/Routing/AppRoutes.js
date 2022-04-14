a4ffbce58bbd6610c3b0605adea814854a16f6c8
albertocruz6 <alberto.cruz6@upr.edu>
2022-04-14 18:44:11 -0400

    Revert routing conditionals in props parameter

--- frontend/src/Routing/AppRoutes.js
+++ frontend/src/Routing/AppRoutes.js
@@ -9,9 +9,9 @@ export default function AppRoutes() {
         <BrowserRouter >
             <Routes>
                 <Route path='/' element={<Home />} />
-                <Route path="/register" element={<Register />} />
+                <Route path="/register" element={<Register condition={false} />} />
                 <Route path="/resources" element={<Resources />}/>
-                <Route path="/login" element={<Register condition={true} />} />
+                <Route path="/login" element={<Register condition={false} />} />
             </Routes>
         </BrowserRouter>
     )
