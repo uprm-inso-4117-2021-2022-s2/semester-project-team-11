f7433588633085e968de7b8f05089978b32fafde
albertocruz6 <alberto.cruz6@upr.edu>
2022-05-05 20:03:54 -0400

    Saving progress in indicators

--- frontend/src/components/Indicators/Indicators.js
+++ frontend/src/components/Indicators/Indicators.js
@@ -1,16 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import NavbarComponent from '../Navbar/Navbar';
+import { BsSearch, BsFillArrowRightCircleFill, BsPlusCircleFill } from 'react-icons/bs';
+import { indicatorRequest } from '../../requests/FinnHub-Requests';
+import { hosts } from '../../config/hosts';
 import './Indicators.css'
 
 const Indicators = () => {
-  return (
-    <>
-    <NavbarComponent/>
-    <div className='indicators-container'>
-        <p>Indicators</p>
-    </div>
-    </>
-  )
+    const [indicators, setIndicators] = useState({})
+    const [loading, setLoading] = useState(false)
+    const [searchKeyword, setSearchKeyword] = useState('')
+    const [searchIndKeyword, setSearchIndKeyword] = useState('')
+    const [title, setTitle] = useState('')
+    const [userStocks, setUserStocks] = useState([])
+
+
+    useEffect(() => {
+        const storage = localStorage.getItem('indicatorsState')
+        const restore = localStorage.getItem('fromIndicatorDetails')
+        if(storage && restore){
+            const state = JSON.parse(storage)
+            setIndicators(state.indicators)
+            setSearchKeyword(state.searchKeyword)
+            setSearchIndKeyword(state.searchIndKeyword)
+        }else{
+            setLoading(true)
+            setTitle('Indicators')
+            indicatorRequest('', '', setIndicators)
+        }
+        fetchUserStocks()
+        localStorage.removeItem('indicatorsState')
+        localStorage.removeItem('fromIndicatorDetails')
+    }, [])
+
+
+    const fetchUserStocks = async () => {
+        const userid = localStorage.getItem('userid')
+        if(!userid){ return ; }
+
+        try{
+            const response = await fetch(`${hosts['heroku']}/stocker/stock/savedStocks/${userid}`)
+            const data = await response.json()
+            const stocks = data.map(stock => stock.symbol[0])
+            setUserStocks(stocks)
+        }
+        catch(e){
+            console.log("Error");
+            localStorage.clear();
+            console.log(e)
+        }
+    }
+
+    const fetchIndicatorSearch = () => {
+        const title = searchKeyword === '' ? 'Indicators' : `Search for indicators of '${searchKeyword}'`
+        resetIndicatorStates()
+        setTitle(title)
+        setLoading(true)
+        indicatorRequest(searchKeyword, searchIndKeyword, setIndicators)
+    }
+
+    const resetIndicatorStates = () => {
+        setIndicators({})
+    }
+    return (
+        <>
+        <NavbarComponent  nav={localStorage.getItem('userid') ? false : true }/>
+        <div className='indicators-container'>
+            <h3 style={{paddingLeft: '15%'}}>{ title }</h3>
+        </div>
+        </>
+    )
 }
 
 export default Indicators
--- frontend/src/requests/FinnHub-Requests.js
+++ frontend/src/requests/FinnHub-Requests.js
@@ -70,7 +70,7 @@ export async function stockProfile(symbol, setFunction) {
         },
     };
     try {
-
+        
         const response = await fetch(`https://finnhub.io/api/v1/stock/profile2?symbol=${symbol}&token=${process.env.FINNHUB_API_KEY}`, request);
         const data = await response.json();
 
@@ -107,5 +107,36 @@ export async function stockQuote(symbol, setFunction) {
     }
 };
 
+
+export async function indicatorRequest(symbol, indicator, setFunction) {
+    const request = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    
+    // Helper method
+    const getUNIXDate = () => {
+        const date = new Date()
+        return Math.floor(date.getTime()/1000)
+    }
+
+    try {
+        const to = getUNIXDate()
+        const from = to - 20*60
+        const response = await fetch(`https://finnhub.io/api/v1/indicator?symbol=${symbol}&resolution=D&from=${from}&to=${to}&indicator=${indicator}&token=${process.env.FINNHUB_API_KEY}`, request);
+        const data = await response.json();
+
+        if (data) setFunction(data);
+        else {
+            console.log(data);
+        }
+    }
+    catch (e) {
+        console.log("Error");
+        console.log(e)
+    }
+};
 
 
